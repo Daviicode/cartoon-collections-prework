454a15959317cfe5d4045752b0094e895c12ae3b
David zhang <d.ydzdavid@yahoo.com>
2019-03-07 14:06:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,16 +14,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(long_planeteer_calls)
+def long_planeteer_calls(calls)
   # code an argument here
   # Your code here
-  answer = false
-  long_planeteer_calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
-    answer
-  end
+  calls.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese (cheese)
